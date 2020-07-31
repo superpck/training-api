b3cb641f32e1fbc3f3a500d96cb161e50c74fa21
Pornchai Khamperngjai <pck@Pornchais-MacBook-Pro.local>
2020-08-01 01:53:57 +0700

    remove vulnerabilities

--- src/app.ts
+++ src/app.ts
@@ -58,17 +58,6 @@ app.decorate("authenticate", async (request, reply) => {
   }
 });
 
-app.register(require('fastify-knexjs'), {
-  client: process.env.DB_CLIENT,
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: +process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }
-});
-
 // connection =========================================
 app.register(require('./plugins/db'), {
   connection: {
